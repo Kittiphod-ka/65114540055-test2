d28848285898dff6835158be23d5681178492e06
kitti <kittiphod.ka.65@ubu.ac.th>
2024-12-09 01:49:28 +0700

    update

--- mycourse/course/views.py
+++ mycourse/course/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from course.models import course
+
+
 # Create your views here.
+
+def course_list(request):
+    courses = course.objects.all()
+    return render(request ,'course_list.html', {"courses": courses})
